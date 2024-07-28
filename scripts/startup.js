6b34057f92a30b853d6f0e53a9a48519ae28febf
tobenot <2818373670@qq.com>
2024-07-28 17:00:08 +0800

    feat: 制作者的话和卡包介绍

--- scripts/startup.js
+++ scripts/startup.js
@@ -78,8 +78,21 @@ async function initializeApp() {
 
 function initModal() {
 	const mainMenuButtons = {
-		creatorButton: "制作者的话内容...",
-		changelogButton: "更新日志内容...",
+		creatorButton: `
+        <p>一直想详细解释心灵世界相关的内容和概念，让大家了解一下心界的伟岸一角。<strong>《不止于纸上的故事：幻灭篇》</strong>就是一次尝试。“幻灭”意味着希望之火虚妄，即便如此，圣人，也即战士们，也会一直向前，一直战斗。</p>
+        <p>本作是一个抽卡做选择的游戏，需要构筑各种方法论来尽可能在最终的高考取得更好的成绩。也许像Rougelike。</p>
+        <p>我会尽力把游戏性做的好玩。同时本作偏向于严肃游戏类型，也力求对于高三生有用。</p>
+        <p><strong>鸣谢与我交流本作的玩家们：</strong>诗学者 半夏</p>
+        <p><strong>致谢与我交流心界的朋友们：</strong>Cecilia 共鸣者 诗学者 段</p>
+        <p><strong>敬谢大自然：</strong>心界三角海 无垠草地 银色星空</p>
+        <p><strong>作者：</strong>苏敬峰/tobenot</p>
+    `,
+		changelogButton:  `
+        <p>只有比较大的更新在这里展示</p>
+        <p><strong>2024年7月28日</strong> 做了主菜单</p>
+        <p><strong>2024年7月25日</strong> 抽卡和数值框架搭好</p>
+        <p><strong>2024年7月23日</strong> 开坑！</p>
+    `,
 	};
 
 	const popupModal = document.getElementById("popup-modal");
@@ -96,12 +109,6 @@ function initModal() {
 
 	closePopup.addEventListener("click", () => {
 		popupModal.style.display = "none";
-	});
-
-	window.addEventListener("click", (event) => {
-		if (event.target == popupModal) {
-			popupModal.style.display = "none";
-		}
 	});
 }
 
@@ -126,9 +133,11 @@ function initMainMenu() {
 function loadCardList() {
 	const cardListContent = document.getElementById("card-list-content");
 	const cards = [
-		{ name: "卡包1", description: "这是卡包1的内容..." },
-		{ name: "卡包2", description: "这是卡包2的内容..." },
-		{ name: "卡包3", description: "这是卡包3的内容..." },
+		{ name: "基础卡包（完善中）", description: "所有人的高三都是这样的……<br><br>含开局介绍卡，日常的所有课程，基本的可决策的时机，在几个特定的时间点会有大考验，最终以高考结束游戏。" },
+    { name: "方法论中毒卡包（制作中）", description: "有很多人会热衷于寻找方法论，就好像玩文明5的时候先点科技再出兵。<br><br>解锁学乎APP，里面有一大堆各种各样的方法论，可以在实践中慢慢养成。" },
+		{ name: "自学卡包（制作中）", description: "如果你问我为什么，我只会说，我需要更多的时间……<br><br>初始解锁自学方法论，如果有‘方法论中毒’卡包，可以解锁学乎APP中自学相关的方法论。" },
+    { name: "心界开拓卡包（制作中）", description: "生而为神，自诩圣人。<br><br>需要‘方法论中毒’卡包，有‘记忆宫殿’方法论之后可以在学乎APP上刷到‘漫游想象世界’，以开启心灵世界事件链。<br><br>包含心界开拓初期到心棱域军队时期之前的全部历史事件。" },
+		{ name: "飞雁一中的同学卡包（制作中）", description: "小心情伤。<br><br>包含同桌克里琴思、前桌艾琳，前斜桌雷思丽三位同学的日常和感情线。<br><br><i>至于为什么他们的名字是英文译名，请期待《不止于纸上的故事：童年篇》。</i>" },
 	];
 
 	cardListContent.innerHTML = ""; // Clear existing content
